421d61341ee171c4c14189bf637590ba4655db8a
ashishranjanjha <ashishranjanjha@tractable.ai>
2019-03-24 23:42:17 +0000

    addign post request fucntionality

--- api.py
+++ api.py
@@ -1,22 +1,30 @@
 import json
 from recipe_allocator import defaults_recipe_allocation
-from flask import Flask
+from flask import Flask, request
 import configparser
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
-config.read('config.ini')
-
-orders_data_path = config['ORDERS']['file_path']
-stock_data_path = config['STOCKS']['file_path']
 
 
-@app.route('/')
-def get_recipe_allocation(orders_data_path=orders_data_path, stock_data_path=stock_data_path):
+@app.route('/static/')
+def get_static_recipe_allocation():
+    config.read('config.ini')
+    orders_data_path = config['ORDERS']['file_path']
+    stock_data_path = config['STOCKS']['file_path']
     with open(orders_data_path) as f:
         recipe_orders_data = json.load(f)
     with open(stock_data_path) as f:
         recipe_stock_data = json.load(f)
+    return str(defaults_recipe_allocation(orders_data=recipe_orders_data, stock_data=recipe_stock_data, debug=False))
+
+
+@app.route('/dynamic/', methods=["POST"])
+def get_dynamic_recipe_allocation():
+    # print(request.get_json())
+    input_json = request.get_json()
+    recipe_orders_data = input_json['orders']
+    recipe_stock_data = input_json['stocks']
     return str(defaults_recipe_allocation(orders_data=recipe_orders_data, stock_data=recipe_stock_data, debug=False))
 
 
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 container_name=recipe_allocation_${USER}
 
-eval "docker run -d -p 5000:5000 ${container_name}"
+eval "docker run -v $(pwd):/app -d -p 5000:5000 ${container_name}"
