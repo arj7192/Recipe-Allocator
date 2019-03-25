d8207963ede5e1bf37f052f8bc1c4e511418e67c
ashishranjanjha <ashishranjanjha@tractable.ai>
2019-03-25 00:54:32 +0000

    adding comments to code

--- recipe_allocator.py
+++ recipe_allocator.py
@@ -6,14 +6,33 @@ config = configparser.ConfigParser()
 
 
 def r_to_num(r):
+    """
+    maps num_recipes string to numerals (this function can be extended for more number of recipes)
+    :param r: num_recipes in plain english
+    :return: num_recipes in integer format
+    """
     return {'two_recipes': 2, 'three_recipes': 3, 'four_recipes': 4}.get(r)
 
 
 def p_to_num(p):
+    """
+    maps portions string to numerals (this function can be extended for more number of portions)
+    :param p: num_portions in plain english
+    :return: num_protions in integer format
+    """
     return {'two_portions': 2, 'four_portions': 4}.get(p)
 
 
 def fetch_portion(stock_list, num_recipe, num_portion, quantity):
+    """
+    Updates the stocks_list after fetching quantity*num_portion*num_recipe portions from the list under
+    the constraints to be followed for defaults users
+    :param stock_list: list of stock numbers for recipes at any given time
+    :param num_recipe: number of recipes that a particular (defaults) customer has requested
+    :param num_portion: number of portions of these recipes that the particular (defaults) customer has requested
+    :param quantity: number of such customers
+    :return: updated stock list after allocating / removing the requested portions.
+    """
     sorted_stock_list = np.array(sorted(stock_list))
     stock_index = 0
     while quantity > 0:
@@ -30,6 +49,13 @@ def fetch_portion(stock_list, num_recipe, num_portion, quantity):
 
 
 def defaults_recipe_allocation(orders_data, stock_data, debug=False):
+    """
+    Algorithm that checks if the stock inventory is sufficient to serve defaults users
+    :param orders_data: dictionary containing recipe orders data for different num_recipes and num_portions
+    :param stock_data: dictionary containing recipe stock data for all the recipes availbale in the inventory
+    :param debug: flag to check the internal working of the algorithm
+    :return: True if inventory if sufficient else False
+    """
     recipe_portion_quantity_tuple = [(r_to_num(r), p_to_num(p), orders_data[r][p]) for r in orders_data.keys() for p in
                                      orders_data[r].keys()]
     recipe_portion_quantity_tuple = sorted(recipe_portion_quantity_tuple, key=lambda c: (c[0], c[1]), reverse=True)
@@ -54,7 +80,6 @@ def defaults_recipe_allocation(orders_data, stock_data, debug=False):
         stock_list = fetch_portion(stock_list, r, p, q)
         if stock_list is None:
             return False
-            break
         if debug:
             print(f'recipe stock list after iteration {i+1}')
             print(f'iteration {i+1} = {r} recipes, {p} portions, {q} customers')
